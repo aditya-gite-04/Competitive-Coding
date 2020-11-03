56d8cb3fe208f82c432f4b58b8666d346b5a9f99
aditya-gite-04 <55936621+aditya-gite-04@users.noreply.github.com>
2020-11-03 20:08:50 +0530

    Add files via upload

---
+++ hollowrect.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+    for(int i=1; i<=row; i++)
+    {
+        if(i==1)
+        {
+            for(int j=1; j<=col; j++)
+                cout<<"*";
+        }
+        else if(i==row)
+        {
+            for(int j=1; j<=col; j++)
+                cout<<"*";
+        }
+        else
+        {
+            for(int j=1; j<=col; j++)
+            {
+                if(j==1 || j==col)
+                {
+                    cout<<"*";
+                }
+                else
+                {
+                    cout<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
